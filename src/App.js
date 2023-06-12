a5656b290698c2ee0a8a8231eed7e21294e0daff
Un-Knwn-me <raghul9510@gmail.com>
2023-06-12 20:17:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import { Badge, Button, Card, Container, Nav, NavDropdown, Navbar, Row } from 'react-bootstrap';
+import { Badge, Button, Card, Col, Container, Nav, NavDropdown, Navbar, Row } from 'react-bootstrap';
 import { data } from './data';
 import { useState } from 'react';
 import RatingStars from 'react-rating-stars-component';
+import { BsFillCartFill } from "react-icons/bs";
 
 function App() {
-
   const [cartCount, setCartCount] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartData] = useState(data);
 
-  const handleAddToCart = () => {
+  const handleAddToCart = (id) => {
     setCartCount((prevCount) => prevCount + 1);
+    setCartItems((prevItems) => [...prevItems, id]);
+  };
+
+  const handleRemoveFromCart = (id) => {
+    setCartCount((prevCount) => prevCount - 1);
+    setCartItems((prevItems) => prevItems.filter((itemId) => itemId !== id));
+  };
+
+  const isItemInCart = (id) => {
+    return cartItems.includes(id);
   };
 
   return (
@@ -28,16 +40,16 @@ function App() {
                 <NavDropdown.Item href="#action/3.3">New Arrivals</NavDropdown.Item>
               </NavDropdown>
             </Nav>
-          <div className="cart">
-            <Button variant="outline-dark">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16">
-                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
-              </svg>
-              <i> </i> Cart<span className="badge bg-dark text-white ms-1 rounded-pill">{cartCount}</span>
-            </Button>
-          </div></Container>
+            <div className="cart">
+              <Button variant="outline-dark">
+                <BsFillCartFill />
+                <b className="ms-1"> Cart - </b>
+                <b className="ms-1 rounded-pill">{cartCount}</b>
+              </Button>
+            </div>
+          </Container>
         </Navbar>
-    
+
         <div className="bg-dark py-5">
           <Container>
             <div className="text-center text-white">
@@ -51,17 +63,35 @@ function App() {
       <div className="shop">
         <Container>
           <Row xs={1} md={4} className="g-4">
-            {data.map((item, idx) => (
-              <Shopcard
-                key={idx}
-                idx={idx}
-                name={item.name}
-                usd={item.usd}
-                usd1={item.usd1}
-                batch={item.batch}
-                rating={item.rating}
-                handleAddToCart={handleAddToCart}
-              />
+            {cartData.map((item, idx) => (
+              <Col key={idx}>
+                <Card>
+                <Badge className='position-absolute top-0 end-0' bg="dark">{item.batch}</Badge>
+                  <Card.Img variant="top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"/>
+                  <Card.Body>
+                    <Card.Title className="center">{item.name}</Card.Title>
+                    <Card.Text>
+                      <p className="center">
+                        <RatingStars count={5} size={24} value={item.rating} edit={false} activeColor="#ffd700" />
+                      </p>
+                      <div className="center">
+                        <span className="strike">{item.usd1}</span>{item.usd}
+                      </div>
+                    </Card.Text>
+                    <div className="center">
+                      {isItemInCart(idx) ? (
+                        <Button variant="outline-dark" key={idx} onClick={() => handleRemoveFromCart(idx)}>
+                          Remove from Cart
+                        </Button>
+                      ) : (
+                        <Button variant="outline-dark" key={idx} onClick={() => handleAddToCart(idx)}>
+                          Add to Cart
+                        </Button>
+                      )}
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
             ))}
           </Row>
         </Container>
@@ -69,44 +99,9 @@ function App() {
       <br /><br />
       <div className="bg-dark py-5">
         <Container>
-          <h3 className='text-center lead fw-normal text-white mb=0'>Copyrights © 2023</h3>
+          <h3 className="text-center lead fw-normal text-white mb-0">Copyrights © 2023</h3>
         </Container>
       </div>
-    </div>
-  );
-}
-
-function Shopcard(props) {
-  return (
-    <div className="cardbox">
-      <Card className="col mb-4">
-        <Card.Img variant="top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" />
-        <Card.ImgOverlay>
-          <h6 className="position-absolute top-0 end-0">
-            <Badge bg="dark">{props.batch}</Badge>
-          </h6>
-        </Card.ImgOverlay>
-        <Card.Body className="bodybox">
-          <Card.Title>{props.name}</Card.Title>
-          <Card.Text>
-            <p className='rating'>
-          <RatingStars
-              count={5}
-              size={24}
-              value={props.rating}
-              edit={false}
-              activeColor="#ffd700"
-            /></p>
-            <div>
-              <span className="strike">{props.usd1}</span> {props.usd}
-            </div>
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer className='bg-transparent border-top-0 p-4 pt-0 rating'>
-          {/* <Button variant="outline-dark" onClick={props.handleAddToCart}>Add to cart</Button> */}
-          <a className='btn btn-outline-dark mt-auto' href='#' onClick={props.handleAddToCart}>Add to cart</a>
-        </Card.Footer>
-      </Card>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -1,54 +1,47 @@
 export const data = [
     {
       name: "Fancy Product",
-      usd: "$40.00-$80.00",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      usd: "$40.00-$80.00"
     },
     {
       name: "Special Item",
       usd1: "$20.00",
       usd: "$80.00",
       rating: "5",
-      batch: "Sale",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      batch: "Sale"
     },
     {
       name: "Sale Item",
       usd1: "$50.00",
       usd: "25.00",
-      batch: "Sale",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      batch: "Sale"
     },
     {
       name: "Popular Item",
       usd: "$40.00",
-      rating: "5",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      rating: "5"
     },
     {
       name: "Sale Item",
       usd: "$25.00",
       usd1: "$50.00",
-      batch: "Sale",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      batch: "Sale"
     },
     {
       name: "Fancy Product",
-      usd: "$120.00-$280.00",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      usd: "$120.00-$280.00"
     },
     {
       name: "Special Item",
       usd: "$18.00",
       usd1: "20.00",
       batch: "Sale",
-      rating: "5",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      rating: "5"
     },
     {
       name: "Popular Item",
       usd: "$40.00",
-      rating: "5",
-      img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+      rating: "5"
     }
+
   ]
